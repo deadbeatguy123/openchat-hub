b5d5ec1d8f769c3755a8e1088229a11b4e8a88ef
Anjeiro <banguis.edward1@gmail.com>
2026-05-07 09:23:39 +0800

    Delete src/routes/api/index.js

--- src/routes/api/index.js
+++
@@ -1,5 +0,0 @@
-// This file must exist in the repo before build for Vercel to recognize it
-export default async function handler(req, res) {
-  const { default: serverHandler } = await import("../dist/server/server.js");
-  return serverHandler(req, res);
-}
